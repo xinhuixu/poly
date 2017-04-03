221ef4ccdb1013ab307605522dd5ba6523d6cb6f
Xinhui <x.xu.1102@gmail.com>
2017-04-03 15:36:39 -0400

    start

--- draw.py
+++ draw.py
@@ -3,16 +3,46 @@ from matrix import *
 from math import *
 
 def add_polygon( points, x0, y0, z0, x1, y1, z1, x2, y2, z2 ):
-    pass
+    add_point(points, x0, y0, z0)
+    add_point(points, x1, y1, z1)
+    add_point(points, x2, y2, z2)
+    print 'ADD_POLY'
 
-def draw_polygons( points, screen, color ):
-    pass
+def draw_polygons( matrix, screen, color ):
+    if len(matrix) < 3:
+        print 'Need at least 3 points to draw POLYGON'
+        return
+    
+    point = 0
+    while point < len(matrix) - 2:
+        draw_line( int(matrix[point][0]),
+                   int(matrix[point][1]),
+                   int(matrix[point+1][0]),
+                   int(matrix[point+1][1]),
+                   screen, color)    
+        draw_line( int(matrix[point+1][0]),
+                   int(matrix[point+1][1]),
+                   int(matrix[point+2][0]),
+                   int(matrix[point+2][1]),
+                   screen, color)    
+        draw_line(  int(matrix[point+2][0]),
+                   int(matrix[point+2][1]),
+                   int(matrix[point][0]),
+                   int(matrix[point][1]),
+                   screen, color)
+        point+= 3
+    
 
 def add_box( points, x, y, z, width, height, depth ):
     x1 = x + width
     y1 = y - height
     z1 = z - depth
 
+    add_polygon(points, x, y, z, x, y1, z, x1, y, z)
+    add_polygon(points, x1, y1, z, x, y1, z, x1, y, z)
+    add_polygon(points, 
+
+'''
     #front
     add_edge(points, x, y, z, x+2, y+2, z+2)
     add_edge(points, x, y1, z, x+2, y1+2, z+2)
@@ -24,6 +54,7 @@ def add_box( points, x, y, z, width, height, depth ):
     add_edge(points, x, y1, z1, x+2, y1+2, z1+2)
     add_edge(points, x1, y, z1, x1+2, y+2, z1+2)
     add_edge(points, x1, y1, z1, x1+2, y1+2, z1+2)
+'''
 
 def add_sphere( edges, cx, cy, cz, r, step ):
     points = generate_sphere(cx, cy, cz, r, step)
